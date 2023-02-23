2cd31aa89811dc10813ce34d188dc0e1dc56fdb8
benas strauka <benas.strauka@telesoftas.com>
2023-02-23 19:47:38 +0200

    Feature: initialize v2 of the project

---
+++ app/src/main/java/com/example/bk_xsports_app_v2/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.bk_xsports_app_v2.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/example/bk_xsports_app_v2/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.bk_xsports_app_v2.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
